1f9e8147b55c5f416c9d8b82d4aa24b583e7be6e
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-02 12:15:00 +0530

    Add files via upload

---
+++ collection/ArrayList/ArrayList/Program.cs
@@ -0,0 +1,56 @@
+﻿//collection.
+//Dynamic class.
+//in array we can resize the array by using Array.Resize. 
+// if we calling Array.Resize method it will store the new value and destroy the older one.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Auto Resizing
+            // there is no size limitation it will keep rezing.
+            ArrayList al = new ArrayList(2);
+            Console.WriteLine(al.Capacity);
+            //The capacity is an property, and which tells the number of an item is stored under Collection.
+            al.Add(100);
+            Console.WriteLine(al.Capacity);
+            al.Add(200);
+            al.Add(300);
+            al.Add(400);
+            Console.WriteLine(al.Capacity);
+            al.Add(500);
+            Console.WriteLine(al.Capacity);
+
+            foreach(object obj in al)
+                Console.WriteLine(obj + " ");
+            Console.WriteLine();
+
+            //inserting an item.
+            al.Insert(3, 350);
+            foreach (object obj in al)
+                Console.WriteLine(obj + " ");
+            Console.WriteLine();
+
+
+            //remove an item,
+            //remove by using the value.
+            // al.Remove(200);
+
+            // can remove by using a index position also.
+            al.RemoveAt(1);
+            foreach (object obj in al)
+                Console.WriteLine(obj + " ");
+            Console.WriteLine();
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ collection/Hashtable/ArrayList1/Program.cs
@@ -0,0 +1,71 @@
+﻿
+// array and arraylist are stored to access the key for value -> index key.
+//key and value comination--> can stored it as
+//key is predifined value. key->index.
+//index is start with 0 and ends with  numbers of value-1.
+// if we having some 30 of the index and value , if we want to access any one of this index , it is impossible to tell the index of the value.
+// so to overcome this problem we are using Hashtable.
+
+
+//Hash Table: is just like an array list.
+//but it stored the value in key/value formate.
+//in hashtable keys are user defined function.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace ArrayList1
+{
+    class Hashtable1
+    {
+        static void Main(string[] args)
+        {
+            //in hash table also can insert the value like an arraylist, but in arraylist we are only  inserting value.
+            // in hash table we can add an item by using key and value of an item.
+   
+            Hashtable1 hash = new Hashtable1();
+            hash.method();
+
+        }
+        private void method()
+        {
+           Hashtable ht = new Hashtable();
+
+            ht.Add("Eid", 1001);
+            ht.Add("Ename", "abhi");
+            ht.Add("Job", "Manager");
+            ht.Add("salary", 20000);
+            ht.Add("mid", 2002);
+            ht.Add("Email", "abhi12@gmail.com");
+            ht.Add("phone", 9632929545);
+
+            ht.Add("Dname", "Sales");
+            ht.Add("Location", "Banglore");
+            ht.Add("Did", 30);
+
+            // if we want to access only one value we can use this method. object of key.
+            Console.WriteLine(ht["Email"]);
+
+            
+            // if we want access all value at a time , we can use foreach loop.
+            foreach (Object key in ht.Keys)
+                Console.WriteLine(key+ ":" + ht[key]);
+                Console.ReadLine();
+
+            // but here values are not print an sequential order.
+            //why? because -> the given key will internally generate an hash code value and stores it.
+            // cashcode is numeric representaion of a value.
+            Console.WriteLine("Hello".GetHashCode());
+
+        }
+    }
+      
+
+    }
+
