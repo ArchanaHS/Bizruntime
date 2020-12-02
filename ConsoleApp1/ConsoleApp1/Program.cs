077a567f9b58a95a6ef53d295b6f0550a2cfc0d3
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-02 10:55:08 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 20;
+            if (time > 10)
+            {
+                Console.WriteLine("good morning");
+            }
+            else if(time<18)
+            {
+                Console.WriteLine("good day");
+            }
+            else
+            {
+                Console.WriteLine("good evening");
+            }
+            if (45 > 34)
+            {
+                Console.WriteLine(" True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
---
+++ DataTypeapplication/DataTypeapplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataTypeapplication
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: {0}", sizeof(int)); // to get to know exact value of type or variable.
+            // using sizeof() method.
+            Console.WriteLine("Size of char: {0}", sizeof(char));
+            // using sizeof() method.
+            Console.WriteLine("Size of double: {0}", sizeof(double));
+            // using sizeof() method.
+            Console.WriteLine("Size of bool: {0}", sizeof(bool));
+            // using sizeof() method.
+          
+            Console.ReadLine();
+        }
+    }
+}
