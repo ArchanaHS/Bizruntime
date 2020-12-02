36d66b882aaba09374c778307805aae79351b37d
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-02 11:42:22 +0530

    Add files via upload

---
+++ c# exception handling/Errorhandling1/Errorhandling1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Errorhandling1
+{
+    class Exceptionhandling
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            int j = Convert.ToInt32(Console.ReadLine());
+            int result=0;
+            try
+            {
+                 result = i/j;
+            }
+           
+            catch (Exception e)
+            {
+                Console.WriteLine("Divide by zero is not allowed ");
+            }
+            finally
+                {
+                Console.WriteLine("blocked");
+            }
+            Console.WriteLine("result is"+result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c# exception handling/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace ExceptionHandiling
+{
+    class Cexception
+    {
+        public void Display() 
+        {
+
+            try //while executing,define a block of code tested for error.
+            {
+                Console.WriteLine("Enter the 1st number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the 2nd number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+
+            } 
+            catch (DivideByZeroException e) //to define block of code to be executed.
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OutOfMemoryException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("EXECUTED");
+            }
+        }
+
+        public void DisplayThrow(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+            }
+            else
+            {
+                Console.WriteLine("Access granted - You are old enough!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cexception exp = new Cexception();
+            exp.Display();
+            exp.DisplayThrow(16);
+
+        }
+
+    }
+}
