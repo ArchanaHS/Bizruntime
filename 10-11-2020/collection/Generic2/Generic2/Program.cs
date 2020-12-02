2b55e63e7c689e53328a1dbf89acbaa17825c19d
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-02 12:17:31 +0530

    Add files via upload

---
+++ 10-11-2020/collection/Generic2/Generic2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generic2
+{
+    class GenericProgram
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            //creating object for printer class.
+            Printer prt = new Printer();
+
+            prt.Print<int> (100);
+            prt.Print(200);// type infer
+
+            prt.Print<string>("Hello");
+            prt.Print("World!");
+
+        }
+    }
+    class Printer
+    {
+        //Generic Method.
+        public void Print<T>(T data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
+  
---
+++ 10-11-2020/collection/Indexer/Indexer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Indexer
+{
+    class IndexProgram
+    {
+       
+        static void Main(string[] args)
+        {
+                StringDataStore strStore = new StringDataStore();
+                //creating an instance for class StringDataStore.
+
+                strStore[0] = "One";
+                strStore[1] = "Two";
+                strStore[2] = "Three";
+                strStore[3] = "Four";
+                strStore[4] = "Five";
+                strStore[5]= "six"
+
+                for (int i = 0; i < 10; i++)
+                    Console.WriteLine(strStore[i]);
+
+             }
+        }
+    public class StringDataStore
+    {
+        private string[] strArr = new string[10];
+	//class StringDataStore Defines the private strArr array.
+        public string this[int index]
+        {
+            get
+            {
+                 if (index < 0 || index >= strArr.Length)
+
+                    throw new IndexOutOfRangeException("Cannot store more than 10 objects");
+
+                    return strArr[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= strArr.Length)
+
+                    throw new IndexOutOfRangeException("Cannot store more than 10 objects"); 
+
+                    strArr[index] = value;
+            }
+        }
+    }
+}
