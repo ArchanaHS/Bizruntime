4a7ca2c076c0f45cea5ec2cb58895699ee1ed4b5
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-02 11:09:22 +0530

    Add files via upload

---
+++ TypeConvertion/TypeConvertion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypeConvertion
+{
+    class Conversion// class declaration.
+    {
+
+        static void Main(string[] args)
+        {
+            double d = 5673.74; // value intialization.
+            int i; // variable declaration.
+
+            // cast double to int.
+            i = (int)d; // type casting.
+            Console.WriteLine(i);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
+
---
+++ variable/variable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable declaration.
+            short a;
+            int b;
+            double c;
+            string name;
+
+            /* actual initialization */
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            // acceptong value from the user.
+            Console.WriteLine("user name");
+           name= Convert.ToString(Console.ReadLine());
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+        
+           
+            
+
+    }
+}
