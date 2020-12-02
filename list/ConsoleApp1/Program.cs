f180475771e7578fc076ee398def2f864502e1ed
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-02 13:01:22 +0530

    Add files via upload

---
+++ list/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Array and arraylist.
+            // fixed size <-> variable size.
+            // not possible to insert an item <-> possible to instert an item.
+            //not possible to delete an item  <-> possible to delete an item.
+
+            //if we want to declare an arraylist means we have to call namespace called System.Collection.
+
+            //creating a list of string.
+            var salmons = new List<string>();
+            salmons.Add("abc\n");
+            salmons.Add("def\n");
+            salmons.Add("ghi\n");
+
+            //iterate through the list.
+            foreach (var salmon in salmons)
+            {
+                Console.Write(salmon + "");
+            }
+        }
+    }
+}
+
