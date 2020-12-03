0d3b215517eb270fd3671fdeafb6cbc218a3460d
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-03 11:53:48 +0530

    Add files via upload

---
+++ ThreadLocking/ThreadLocking/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ThreadLockingProgramming
+{
+    class ThreadLocking
+    {
+        public void Display()
+        {
+            lock (this)
+            {
+                Console.Write("[CSharp is an ");
+                Thread.Sleep(3000);
+                Console.WriteLine("Object Oriented language..]");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadLocking obj = new ThreadLocking();
+
+            //obj.Display();
+            //obj.Display();
+            //obj.Display();
+            Thread t1 = new Thread(obj.Display);
+            Thread t2 = new Thread(obj.Display);
+            Thread t3 = new Thread(obj.Display);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ThreadPriority/ThreadPriority/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ThreadLocking
+    {
+
+        class ThreadPriorities
+        {
+            static long Count1, Count2;
+            public static void IncrementCount1()
+            {
+                
+                while (true)
+                    Count1 += 1;
+
+
+            }
+            public static void IncrementCount2()
+            {
+                while (true)
+                    Count2 += 1;
+
+
+            }
+             static void Main()
+            {
+                Thread t1 = new Thread(IncrementCount1);
+                Thread t2 = new Thread(IncrementCount2);
+
+                t1.Priority = ThreadPriority.Lowest;
+                t2.Priority = ThreadPriority.Highest;
+
+                t1.Start();
+                t2.Start();
+
+            Console.WriteLine("The Main Method are going to sleep...");
+            Thread.Sleep(3000);
+            Console.WriteLine("The main Thread woke up...");
+
+
+            try
+            {
+                t1.Abort();
+                t2.Abort();
+
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+
+
+            t1.Join();
+                t2.Join();
+
+            Console.WriteLine("The Count1 :" + Count1);
+            Console.WriteLine("The Count2 :" + Count2);
+
+            Console.ReadLine();
+
+
+
+        }
+
+        }
+    }
+
+
