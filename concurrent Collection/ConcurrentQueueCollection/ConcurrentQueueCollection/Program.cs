b0e3ca2ecb432b0efe5e9e59044d122d6b519db9
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-03 11:58:01 +0530

    Add files via upload

---
+++ concurrent Collection/ConcurrentQueueCollection/ConcurrentQueueCollection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+class CQ_EnqueueDequeuePeek
+{
+    
+    static void Main()
+    {
+        //creating  instance for ConcurrentQueue collection.
+        ConcurrentQueue<int> cq = new ConcurrentQueue<int>();
+
+        //adding an item into cq
+        for (int i = 0; i < 10000; i++)
+        {
+            cq.Enqueue(i);
+        }
+
+        
+        int result;
+        if (!cq.TryPeek(out result))
+        {
+            Console.WriteLine("CQ: TryPeek failed when it should have succeeded");
+        }
+        else if (result != 0)
+        {
+            Console.WriteLine("CQ: Expected TryPeek result of 0, got {0}", result);
+        }
+
+        int outerSum = 0;
+        
+        Action action = () =>
+        {
+            int localSum = 0;
+            int localValue;
+            while (cq.TryDequeue(out localValue)) localSum += localValue;
+            Interlocked.Add(ref outerSum, localSum);
+        };
+
+        //concurrently consuming an action
+        Parallel.Invoke(action, action, action, action);
+
+        Console.WriteLine("outerSum = {0}, should be 49995000", outerSum);
+    }
+}
---
+++ delegates/DelegatesFunction/DelegatesFunction/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+delegate int NumberChanger(int n);
+namespace DelegatesFunction
+{
+    class Program
+    {
+        static int num = 1;
+        public static int AddNum(int P)
+        {
+            num += P;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            //create instance of delegate.
+            NumberChanger nc;
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+            //nc1(24);
+            //Console.WriteLine("Value of num: {0}", getNum());
+            nc2(50);
+            Console.WriteLine("Value of num :{0}", getNum());
+
+            nc = nc1;
+            nc += nc2;
+            //multicast delegate.
+            nc(5);
+            Console.WriteLine("Value of num :{0}", getNum());
+            Console.ReadLine();
+
+
+        }
+    }
+}
