0c3cb15b78270235a33b28db76004048a1227d9d
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-04 12:23:06 +0530

    Add files via upload

---
+++ EmploymentProject/EmploymentProject/EmployeeDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EmploymentProject
+{
+    class EmployeeDetails
+    {
+        
+            [key]
+            public int EmpId { get; set; }
+
+            [Column(TypeName = "nvarchar(50)")]
+            public string EmpName { get; set; }
+
+            [Column(TypeName = "nvarchar(50)")]
+            public string ComanyName { get; set; }
+
+            [Column(TypeName = "nvarchar(50)")]
+            public int ComanyId { get; set; }
+
+            [Column(TypeName = "nvarchar(50)")]
+            public string Designation { get; set; }
+
+            [Column(TypeName = "nvarchar(50)")]
+            public string Department { get; set; }
+
+        
+       
+    }
+}
---
+++ LinqJsonProject/LinqJsonProject/linqJsonPC.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqJsonProject
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string AcademicYear { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+    public class Course
+    {
+        public int CId { get; set; }
+        public string CName { get; set; }
+    }
+
+    public class StudentDatabase:List<Student>
+    {
+        public StudentDatabase()
+        {
+            Add(new Student()
+            {
+                Id = 1,
+                Name = "Akash",
+                AcademicYear = "First",
+                Courses = new List<Course>()
+                {
+                 new Course() {CId=101,CName="C++" },
+                 new Course() {CId=102,CName="C" },
+                 new Course() {CId=103,CName="Visual Basic" }
+                }
+
+            });
+            Add(new Student()
+            {
+                Id = 2,
+                Name = "Anjana",
+                AcademicYear = "Second",
+                Courses = new List<Course>()
+                {
+                 new Course() {CId=101,CName="C++" },
+                 new Course() {CId=102,CName="C" },
+                 new Course() {CId=103,CName="Visual Basic" }
+                }
+
+            });
+            Add(new Student()
+            {
+                Id = 3,
+                Name = "Ankitha",
+                AcademicYear = "Third",
+                Courses = new List<Course>()
+                {
+                 new Course() {CId=101,CName="C++" },
+                 new Course() {CId=102,CName="C" },
+                 new Course() {CId=103,CName="Visual Basic" }
+                }
+
+            });
+
+
+
+
+        }
+    }
+}
