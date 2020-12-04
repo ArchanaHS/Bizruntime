1920f7f340337c66c3abba5112c708fcfdda320f
ArchanaHS <75110994+ArchanaHS@users.noreply.github.com>
2020-12-04 12:24:58 +0530

    Add files via upload

---
+++ socket programing/ClientPrograming/ClientPrograming/Program.cs
@@ -0,0 +1,91 @@
+﻿// A C# program for Client 
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+
+	class ClientProgram
+	{
+
+		
+		static void Main(string[] args)
+		{
+			ExecuteClient();
+		}
+
+	
+		static void ExecuteClient()
+		{
+
+			try
+			{
+
+				
+				IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());//creating instance of hostentry
+				IPAddress ipAddr = ipHost.AddressList[0];//getting the ip address
+				IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);//forms a connection point to a service.
+
+				//creating socket.
+				Socket sender = new Socket(ipAddr.AddressFamily,
+						SocketType.Stream, ProtocolType.Tcp);
+
+				try
+				{
+
+					//connecting to the socket
+					sender.Connect(localEndPoint);
+
+					
+					Console.WriteLine("Socket connected to -> {0} ",
+								sender.RemoteEndPoint.ToString());
+
+					// sending information to the server.
+					byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+					int byteSent = sender.Send(messageSent);
+
+					
+					byte[] messageReceived = new byte[1024];
+
+					//receive data
+					int byteRecv = sender.Receive(messageReceived);
+					//printing recieved data
+					Console.WriteLine("Message from Server -> {0}",
+						Encoding.ASCII.GetString(messageReceived,
+													0, byteRecv));
+
+					// close connection
+					sender.Shutdown(SocketShutdown.Both);
+					//method close.
+					sender.Close();
+				}
+
+				
+				catch (ArgumentNullException ane)
+				{
+
+					Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+				}
+
+				catch (SocketException se)
+				{
+
+					Console.WriteLine("SocketException : {0}", se.ToString());
+				}
+
+				catch (Exception e)
+				{
+					Console.WriteLine("Unexpected exception : {0}", e.ToString());
+				}
+			}
+
+			catch (Exception e)
+			{
+
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
+}
---
+++ socket programing/ClientPrograming/SocketPrograming/Program.cs
@@ -0,0 +1,83 @@
+﻿// A C# Program for Server 
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+
+	class ServerProgram
+	{
+
+		
+		static void Main(string[] args)
+		{
+			ExecuteServer();
+		}
+
+		public static void ExecuteServer()
+		{
+			//creating instance of IPHostEntry
+			IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+			IPAddress ipAddr = ipHost.AddressList[0];//getting ip address
+			IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+			//forms a connection
+
+			//creating socket
+			Socket listener = new Socket(ipAddr.AddressFamily,
+						SocketType.Stream, ProtocolType.Tcp);
+
+			try
+			{
+
+				listener.Bind(localEndPoint);
+				//bind method listen the listen()method at the end point.
+			
+				listener.Listen(10);//listen the state.
+
+				while (true)
+				{
+
+					Console.WriteLine("Waiting connection ... ");
+
+					//creating method for accept the listener information.
+					Socket clientSocket = listener.Accept();
+
+				
+					byte[] bytes = new Byte[1024];
+
+					string data = null;
+
+					while (true)
+					{
+						//received data
+						int numByte = clientSocket.Receive(bytes);
+
+						data += Encoding.ASCII.GetString(bytes,
+												0, numByte);
+
+						if (data.IndexOf("<EOF>") > -1)
+							break;
+					}
+
+					Console.WriteLine("Text received -> {0} ", data);
+					byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+					//send message to the server
+					clientSocket.Send(message);
+
+					//receiver closed
+					//method is closed
+					clientSocket.Shutdown(SocketShutdown.Both);
+					clientSocket.Close();
+				}
+			}
+
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
+}
